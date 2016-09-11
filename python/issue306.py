a06e55adcfd0bbf59e15644d74088a2f302389ed
Jan Larres <jan@majutsushi.net>
2016-09-11 20:49:55 +1200

    Add test for tagbar#306

---
+++ python/issue306.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class B():
+    def meth_on_B():
+        e = 5
+
+        def inner_function():
+            class function_in_class:
+                inner_member = 1
+    b = 2
+
+def foo():
+    c = 3
+
+d = 4
